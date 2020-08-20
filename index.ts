1f9a2c30865a9d58ef866e5c434529e62af6fed1
sl1673495 <949589855@qq.com>
2020-08-20 20:58:17 +0800

    feat: 使用 localforage 存储

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import localforage from "localforage";
+
 export interface CacheFetchOptions {
   /**
    * 生成缓存key的策略，默认策略是直接拼接 url + stringify(body)
@@ -13,7 +15,7 @@ export interface CacheFetchOptions {
   shouldCacheResult?(response: Response): Promise<boolean>;
 }
 
-const STORAGE_PREFIX = "request-cache:";
+const STORAGE_PREFIX = "request-dev-cache:";
 
 export const startCache = ({
   generateKey = defaultGenerateKey,
@@ -37,70 +39,61 @@ export const startCache = ({
     if (!shouldHandleRequest?.(url, options)) {
       return originFetch(...params);
     }
-
     const body = parseBody(options);
-
     const cacheKey = `${STORAGE_PREFIX}${generateKey(url, body)}`;
-    const cacheResult = localStorage.getItem(cacheKey);
-
+    const cacheResult: object | null = await localforage.getItem(cacheKey);
     if (cacheResult) {
-      const result = JSON.parse(cacheResult);
-      log(url, body, result);
-      return new Response(cacheResult);
+      log(url, body, cacheResult);
+      return new Response(JSON.stringify(cacheResult));
     } else {
       const resp = await originFetch(...params);
-      const clonedResponse = resp.clone();
-      const result = await resp.json();
-      const stringifyResult = JSON.stringify(result);
-      if (await shouldCacheResult(clonedResponse)) {
-        localStorage.setItem(cacheKey, stringifyResult);
+      const responseForFilter = resp.clone();
+      if (await shouldCacheResult(responseForFilter)) {
+        const responseForStorage = resp.clone();
+        const result = await responseForStorage.json();
+        await localforage.setItem(cacheKey, result);
       }
-      return new Response(stringifyResult);
-    }
-  };
-
-  const forEachStorage = (callback: (key: string | null) => void) => {
-    const lens = localStorage.length;
-    for (let index = 0; index < lens; index++) {
-      const key = localStorage.key(index);
-      callback(key);
+      return resp;
     }
   };
 
   /**
    * 清除所有缓存
    */
-  (window as any).cleanAllRequestDevCaches = () => {
+  (window as any).cleanAllRequestDevCaches = async () => {
     const targetKeys: string[] = [];
-    forEachStorage((key) => {
+    const keys = await localforage.keys();
+
+    keys.forEach((key) => {
       if (key?.startsWith(STORAGE_PREFIX)) {
         targetKeys.push(key);
       }
     });
     targetKeys.forEach((key) => {
-      localStorage.removeItem(key);
+      localforage.removeItem(key);
     });
   };
 
   /**
    * 根据 url 模糊匹配清除缓存
    */
-  (window as any).cleanRequestDevCache = (url: string) => {
-    forEachStorage((key) => {
+  (window as any).cleanRequestDevCache = async (url: string) => {
+    const keys = await localforage.keys();
+    keys.forEach((key) => {
       if (key?.includes(`${STORAGE_PREFIX}${url}`)) {
-        localStorage.removeItem(key);
+        localforage.removeItem(key);
       }
     });
   };
 };
 
-function log(url, body, result) {
+function log(url: RequestInfo, body: object, result: object) {
   console.groupCollapsed(`接口缓存读取成功 ${url}`);
   console.log("%c 接口参数", "color: #03A9F4; font-weight: bold", body);
   console.log("%c 缓存结果", "color: #4CAF50; font-weight: bold", result);
   console.groupEnd();
 }
 
-function defaultGenerateKey(url, body) {
+function defaultGenerateKey(url: RequestInfo, body: object) {
   return `${url}-${JSON.stringify(body as object)}`;
 }
