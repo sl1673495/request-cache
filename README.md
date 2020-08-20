022cec6b9335fd31cfec8156a65b2ddd2fbfeaab
sl1673495 <949589855@qq.com>
2020-08-20 18:36:21 +0800

    feat: 本地缓存 fetch 结果

---
+++ README.md
@@ -0,0 +1,83 @@
+<h1 align="center">Welcome to request-cache 👋</h1>
+<p>
+  <a href="https://www.npmjs.com/package/request-cache" target="_blank">
+    <img alt="Version" src="https://img.shields.io/npm/v/request-cache.svg">
+  </a>
+  <a href="#" target="_blank">
+    <img alt="License: MIT" src="https://img.shields.io/badge/License-MIT-yellow.svg" />
+  </a>
+</p>
+
+> 本地缓存请求的结果 加快开发速度
+
+## Install
+
+```sh
+npm install request-cache -S
+```
+
+## Usage
+
+### Options
+
+```ts
+interface CacheFetchOptions {
+  /**
+   * 生成缓存key的策略，默认策略是直接拼接 url + stringify(body)
+   */
+  generateKey?: (url: RequestInfo, body: object) => string;
+  /**
+   * 传入 url 和 fetch 选项 判断是否需要缓存
+   */
+  shouldHandleRequest?(url: RequestInfo, requestInit?: RequestInit): boolean;
+  /**
+   * 传入 response 响应对象 判断是否需要缓存
+   */
+  shouldCacheResult?(response: Response): Promise<boolean>;
+}
+```
+
+```js
+import { startCache } from 'request-cache';
+import { omit } from 'lodash'
+
+startCache({
+  generateKey(url, body) {
+    // 可以排除掉一些你不想缓存的参数值
+    return `${url}-${JSON.stringify(omit(body, ['key1', 'key2']))}`;
+  },
+  shouldHandleRequest(url: string) {
+    // 可以规定 /api 开头的请求才缓存
+    return url.startsWith('/api');
+  },
+  shouldCacheResult: async response => {
+    // 过滤掉不想缓存的结果
+    const result = await response.json();
+    return (
+      response.headers.get('content-type') === 'application/json' &&
+      response.status === 200 &&
+    );
+  },
+});
+```
+
+## Run tests
+
+```sh
+npm run test
+```
+
+## Author
+
+👤 **ssh**
+
+- Website: https://ssh-blog.now.sh
+- Github: [@sl1673495](https://github.com/sl1673495)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+---
+
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
---
+++ index.ts
@@ -0,0 +1,106 @@
+export interface CacheFetchOptions {
+  /**
+   * 生成缓存key的策略，默认策略是直接拼接 url + stringify(body)
+   */
+  generateKey?: (url: RequestInfo, body: object) => string;
+  /**
+   * 传入 url 和 fetch 选项 判断是否需要缓存
+   */
+  shouldHandleRequest?(url: RequestInfo, requestInit?: RequestInit): boolean;
+  /**
+   * 传入 response 响应对象 判断是否需要缓存
+   */
+  shouldCacheResult?(response: Response): Promise<boolean>;
+}
+
+const STORAGE_PREFIX = "request-cache:";
+
+export const startCache = ({
+  generateKey = defaultGenerateKey,
+  shouldHandleRequest = () => true,
+  shouldCacheResult = async () => true,
+}: CacheFetchOptions) => {
+  const parseBody = (options?: RequestInit) => {
+    let { body: rawBody } = options ?? {};
+    let body: object;
+    if (typeof rawBody === "string") {
+      body = JSON.parse(rawBody as string) ?? {};
+    } else {
+      body = {};
+    }
+    return body;
+  };
+
+  const originFetch = window.fetch;
+  window.fetch = async (...params) => {
+    const [url, options] = params;
+    if (!shouldHandleRequest?.(url, options)) {
+      return originFetch(...params);
+    }
+
+    const body = parseBody(options);
+
+    const cacheKey = `${STORAGE_PREFIX}${generateKey(url, body)}`;
+    const cacheResult = localStorage.getItem(cacheKey);
+
+    if (cacheResult) {
+      const result = JSON.parse(cacheResult);
+      log(url, body, result);
+      return new Response(cacheResult);
+    } else {
+      const resp = await originFetch(...params);
+      const clonedResponse = resp.clone();
+      const result = await resp.json();
+      const stringifyResult = JSON.stringify(result);
+      if (await shouldCacheResult(clonedResponse)) {
+        localStorage.setItem(cacheKey, stringifyResult);
+      }
+      return new Response(stringifyResult);
+    }
+  };
+
+  const forEachStorage = (callback: (key: string | null) => void) => {
+    const lens = localStorage.length;
+    for (let index = 0; index < lens; index++) {
+      const key = localStorage.key(index);
+      callback(key);
+    }
+  };
+
+  /**
+   * 清除所有缓存
+   */
+  (window as any).cleanAllRequestDevCaches = () => {
+    const targetKeys: string[] = [];
+    forEachStorage((key) => {
+      if (key?.startsWith(STORAGE_PREFIX)) {
+        targetKeys.push(key);
+      }
+    });
+    targetKeys.forEach((key) => {
+      localStorage.removeItem(key);
+    });
+  };
+
+  /**
+   * 根据 url 模糊匹配清除缓存
+   */
+  (window as any).cleanRequestDevCache = (url: string) => {
+    forEachStorage((key) => {
+      if (key?.includes(`${STORAGE_PREFIX}${url}`)) {
+        localStorage.removeItem(key);
+      }
+    });
+  };
+};
+
+function log(url, body, result) {
+  console.groupCollapsed(`接口缓存读取成功 ${url}`);
+  console.log("%c 接口参数", "color: #03A9F4; font-weight: bold", body);
+  console.log("%c 缓存结果", "color: #4CAF50; font-weight: bold", result);
+  console.groupEnd();
+}
+
+function defaultGenerateKey(url, body) {
+  return `${url}-${JSON.stringify(body as object)}`;
+}
