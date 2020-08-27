a6fd4efdf986fb44a9a5edc0b193e03cd3f9b5a4
sl1673495 <949589855@qq.com>
2020-08-27 21:43:31 +0800

    perf: split type file

--- index.ts
+++ index.ts
@@ -1,19 +1,5 @@
 import localforage from "localforage";
-
-export interface CacheFetchOptions {
-  /**
-   * 生成缓存key的策略，默认策略是直接拼接 url + stringify(body)
-   */
-  generateKey?: (url: RequestInfo, body: object) => string;
-  /**
-   * 传入 url 和 fetch 选项 判断是否需要缓存
-   */
-  shouldHandleRequest?(url: RequestInfo, requestInit?: RequestInit): boolean;
-  /**
-   * 传入 response 响应对象 判断是否需要缓存
-   */
-  shouldCacheResult?(response: Response): Promise<boolean>;
-}
+import type { CacheFetchOptions } from "./type";
 
 const STORAGE_PREFIX = "request-dev-cache:";
 
